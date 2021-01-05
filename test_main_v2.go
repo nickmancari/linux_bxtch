48a55b1c13fabd485e0e15c12d75e15cdfb20667
nickmancari <nickjmancari@gmail.com>
2021-01-05 20:52:17 +0000

    testing v2

--- test_main_v2.go
+++ test_main_v2.go
@@ -39,6 +39,7 @@ func results(w http.ResponseWriter, r *http.Request) {
 	}
 	
 	search := r.FormValue("q")
+
 	jq := gojsonq.New().File("./complete_master.json")
         res := jq.Find(search)
         link := fmt.Sprint(res)	
